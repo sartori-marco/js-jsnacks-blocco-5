b690a3d490799812cef7ef7e43964719cb6bab1f
marco sartori <marcosartori98@yahoo.com>
2021-01-18 16:30:47 +0100

    start snack 3

---
+++ Jsnack3/js/script.js
@@ -0,0 +1,79 @@
+// Jsnack3
+// Creare  un array che contiene 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+var zucchine = [
+  {
+    nome : 'nome1',
+    peso: 3,
+    lunghezza: 19
+  },
+
+  {
+    nome : 'nome2',
+    peso: 5,
+    lunghezza: 9
+  },
+
+  {
+    nome : 'nome3',
+    peso: 1,
+    lunghezza: 18
+  },
+
+  {
+    nome : 'nome4',
+    peso: 1,
+    lunghezza: 16
+  },
+
+  {
+    nome : 'nome5',
+    peso: 9,
+    lunghezza: 7
+  },
+
+  {
+    nome : 'nome6',
+    peso: 6,
+    lunghezza: 1
+  },
+
+  {
+    nome : 'nome7',
+    peso: 13,
+    lunghezza: 16
+  },
+
+  {
+    nome : 'nome8',
+    peso: 4,
+    lunghezza: 9
+  },
+
+  {
+    nome : 'nome9',
+    peso: 3,
+    lunghezza: 19
+  },
+
+  {
+    nome : 'nome10',
+    peso: 4,
+    lunghezza: 21
+  }
+];
+
+// divido le zucchine in due array < o > 15
+// creo i due array vuoti che andranno popolati
+
+var zucchineCorte = [];
+var zucchineLunghe = [];
+
+// poi sommo i pesi dei due gruppi
+// creo i due array per le somme
+
+
+
+
+//
